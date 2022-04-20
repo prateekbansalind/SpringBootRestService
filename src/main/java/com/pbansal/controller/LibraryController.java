9a0b535f15a12acdd26e8e59a33b31952e797ca2
Prateek Bansal <prateekbansalind@gmail.com>
2022-04-20 20:38:34 +1200

    Implement add Book REST API Logic

--- src/main/java/com/pbansal/controller/LibraryController.java
+++ src/main/java/com/pbansal/controller/LibraryController.java
@@ -2,6 +2,7 @@ package com.pbansal.controller;
 
 import com.pbansal.repository.ILibraryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,12 +18,16 @@ public class LibraryController {
     @PostMapping("/addBook")
     public ResponseEntity addBookImplementation(@RequestBody Library library){
         library.setId(library.getIsbn()+library.getAisle());
+        String id = library.getId();
         repository.save(library);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Unique", id);
 
         AddResponse addResponse = new AddResponse();
         addResponse.setMessage("Success book is added.");
-        addResponse.setId(library.getId());
+        addResponse.setId(id);
 
-        return new ResponseEntity<AddResponse>(addResponse, HttpStatus.CREATED);
+        return new ResponseEntity<AddResponse>(addResponse,httpHeaders,HttpStatus.CREATED);
     }
 }
