7944046908c197c4b6bd00bd795c8f9249dd9824
Prateek Bansal <prateekbansalind@gmail.com>
2022-04-20 12:05:29 +1200

    create another data entry into mySQL database with JPA methods.

--- src/main/java/com/pbansal/SpringBootRestServiceApplication.java
+++ src/main/java/com/pbansal/SpringBootRestServiceApplication.java
@@ -1,3 +1,4 @@
+
 package com.pbansal;
 
 import com.pbansal.controller.Library;
@@ -6,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootRestServiceApplication implements CommandLineRunner {
@@ -20,8 +23,23 @@ public class SpringBootRestServiceApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String[] args){
-		Library lib = repository.findById("fdsefr343").get();
-		System.out.println(lib.getAuthor());
+		// Retrieve author name based on Id "fdsefr343".
+		Library lib1 = repository.findById("fdsefr343").get();
+		System.out.println(lib1.getAuthor());
+
+		// add another record in the sql database
+		Library lib2 = new Library();
+		lib2.setAisle(123);
+		lib2.setId("XYZ123");
+		lib2.setAuthor("Professor X");
+		lib2.setBook_name("Learn Mutant History");
+		lib2.setIsbn("XYZ");
+		repository.save(lib2);
+		List<Library> allRecords = repository.findAll();
+		for (Library entity : allRecords){
+			System.out.println(entity.getAuthor());
+		}
+
 	}
 
 
