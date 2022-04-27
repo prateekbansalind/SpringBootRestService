2891cf8443e50f4b9a090b726d754860515a0d57
Prateek Bansal <prateekbansalind@gmail.com>
2022-04-27 19:38:43 +1200

    Implement GetBookById Logic

--- src/main/java/com/pbansal/controller/LibraryController.java
+++ src/main/java/com/pbansal/controller/LibraryController.java
@@ -6,37 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class LibraryController {
     @Autowired
     ILibraryRepository repository;
-
     @Autowired
     LibraryService libraryService;
-
-
     @PostMapping("/addBook")
     public ResponseEntity addBookImplementation(@RequestBody Library library){
-
         String id = libraryService.buildId(library.getIsbn(), library.getAisle());
-
         AddResponse addResponse = new AddResponse();
-
         if (!libraryService.checkBookAlreadyExist(id)) {
             library.setId(id);
             repository.save(library);
-
             HttpHeaders httpHeaders = new HttpHeaders();
             httpHeaders.add("Unique", id);
-
-
             addResponse.setMessage("Success book is added.");
             addResponse.setId(id);
-
             return new ResponseEntity<AddResponse>(addResponse,httpHeaders,HttpStatus.CREATED);
         }
         else{
@@ -44,6 +33,17 @@ public class LibraryController {
             addResponse.setId(id);
             return new ResponseEntity<AddResponse>(addResponse,HttpStatus.ACCEPTED);
         }
-
     }
+
+    @GetMapping(value = "/getBook/{id}")
+    public Library getBookByIdImplementation(@PathVariable(value = "id") String id){
+        try {
+            Library lib = repository.findById(id).get();
+            return lib;
+        }
+        catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
