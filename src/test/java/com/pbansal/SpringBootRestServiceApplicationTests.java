42be08f1dfd1e8d4267f45f6889712314ce7aa55
Prateek Bansal <prateekbansalind@gmail.com>
2022-04-29 11:54:40 +1200

    Implement unit test for build Id method.

--- src/test/java/com/pbansal/SpringBootRestServiceApplicationTests.java
+++ src/test/java/com/pbansal/SpringBootRestServiceApplicationTests.java
@@ -1,13 +1,27 @@
 package com.pbansal;
 
+import com.pbansal.service.LibraryService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class SpringBootRestServiceApplicationTests {
 
+	// Dependencies
+	@Autowired
+	LibraryService service;
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	public void checkBuildIdLogic(){
+		String id = service.buildId("ZAC", 786);
+		assertEquals(id, "OLDZAC786");
+		String anotherId = service.buildId("MAN", 123);
+		assertEquals(anotherId, "MAN123");
 	}
 
 }
